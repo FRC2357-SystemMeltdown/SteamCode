784eab80f878f8218c9141ff2c0cdd23e1a1a6e4
Robert <Robert@10.110.0.65>
2017-02-02 16:23:07 -0600

    Created Dashboard Drive Mode Switch (Untested)

--- SteamCode/src/org/usfirst/frc/team2357/robot/Config.java
+++ SteamCode/src/org/usfirst/frc/team2357/robot/Config.java
@@ -6,10 +6,12 @@ package org.usfirst.frc.team2357.robot;
  *
  */
 public class Config {
-	public static boolean driveMode = false;
+	public static boolean driveMode = true;
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-
+	public static void setDriveMode(boolean x){
+		driveMode = x;
+	}
 }
 
